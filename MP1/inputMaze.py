2644c2644a6f4496fecaa027c468e6a28f65a78c
deltadu <dujianlin@berkeley.edu>
2017-09-22 20:20:49 -0500

    got ride of line break

--- MP1/inputMaze.py
+++ MP1/inputMaze.py
@@ -1,4 +1,4 @@
 def inputMaze(name="mediumMaze.txt"):
-	with open(name) as mazeTxt:
-	    maze = [list(line) for line in mazeTxt]
-	return maze
+    with open(name) as mazeTxt:
+        maze = [list(line)[0:-2] for line in mazeTxt]
+    return maze
