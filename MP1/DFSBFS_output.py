313cc0a17ac4ee7bb0d4089cc000a7f51318ef90
ZYJ001 <32210663+ZYJ001@users.noreply.github.com>
2017-09-23 17:53:29 -0500

    DFS/BFS and outputMaze

--- MP1/DFSBFS_output.py
+++ MP1/DFSBFS_output.py
@@ -1,4 +1,6 @@
-def bfs(maze):
+def DFSBFS(maze,search_method):
+    # search_method 0 for dfs
+    # 1 for bfs
     height = len(maze)
     width = len(maze[0])
 
@@ -10,17 +12,14 @@ def bfs(maze):
     # set up the matrix for the path
     path = (height*width) * [None]
 
-    # use queue for BFS implementation
-    # set up the queue
-
-    queue = []
-    queue.insert(0,start)
+    ele_list = []
+    ele_list.insert(0,start)
+   
     #mark as visited
     visited[start[0]*width+start[1]] = True
 
-
-    while queue:
-        curr = queue.pop()
+    while ele_list:
+        curr = ele_list.pop()
         if curr == goal:
             break
 
@@ -28,7 +27,11 @@ def bfs(maze):
         for n in neighbors:
             if n!= None:
                 if visited[n[0]*width+n[1]] == False:
-                    queue.insert(0,n);
+                    if search_method == 0: #stack, insert at back
+                        ele_list.append(n)
+                    else:
+                            #bfs(queue), insert at front
+                        ele_list.insert(0,n)
                     visited[n[0]*width+n[1]] = True
                     path[n[0]*width+n[1]] = curr
 
@@ -40,3 +43,14 @@ def bfs(maze):
 
     return bookkeeping
 
+
+def outputMaze(maze,search_method,filename):
+    path = DFSBFS(maze,search_method)
+    for i in range(1,len(path)):
+        maze[(path[i])[0]][(path[i])[1]] = '.'
+    f = open(filename,"w")
+    for i in range(len(maze)):
+        for j in range(len(maze[0])):
+            f.write(maze[i][j])
+        f.write('\n')
+    f.close()
